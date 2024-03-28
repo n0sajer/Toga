85aed415b2e788a215ef332de1246d0a4268e3ef
Jason Wheelhouse <jason.wheelhouse@gmail.com>
2024-03-28 15:53:58 +1100

    not sure what this commit was

--- helloworld.py
+++ helloworld.py
@@ -10,9 +10,9 @@ def build(app):
     c_input = toga.TextInput(readonly=True)
     f_input = toga.TextInput()
 
-    c_label = toga.Label("Celsius", style=Pack(text_align=RIGHT))
-    f_label = toga.Label("Fahrenheit", style=Pack(text_align=RIGHT))
-    join_label = toga.Label("is equivalent to", style=Pack(text_align=LEFT))
+    c_label = toga.Label("Celsius", style=Pack(text_align=LEFT))
+    f_label = toga.Label("Fahrenheit", style=Pack(text_align=LEFT))
+    join_label = toga.Label("is equivalent to", style=Pack(text_align=RIGHT))
 
     def calculate(widget):
         try:
